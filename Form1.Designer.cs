5cc5ab9aed7d5380a0deaf3ca1bc600694b1dcb3
massimo trovati <massimotrovati92@gmail.com>
2020-07-14 11:32:23 +0200

    Funny-Age-Calculator-Win32
    for new developers

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Funnny_Age_Calculator_Win32
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(26, 54);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Info;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please Input Your BirthDay";
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // Calculate
+            // 
+            this.Calculate.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.Calculate.ForeColor = System.Drawing.SystemColors.Info;
+            this.Calculate.Location = new System.Drawing.Point(16, 80);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(223, 33);
+            this.Calculate.TabIndex = 2;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = false;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Enabled = false;
+            this.dateTimePicker2.Location = new System.Drawing.Point(26, 32);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 3;
+            this.dateTimePicker2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(256, 125);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.Name = "Form1";
+            this.Text = "Funny-Age-Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Funnny_Age_Calculator_Win32
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            //Read The Birthday
+            int year = Convert.ToInt32(dateTimePicker1.Value.Year.ToString());
+            int month = Convert.ToInt32(dateTimePicker1.Value.Month.ToString());
+            int day = Convert.ToInt32(dateTimePicker1.Value.Day.ToString());
+        
+            //Now Time
+
+
+            int yearn = Convert.ToInt32(dateTimePicker2.Value.Year.ToString());
+            int monthn = Convert.ToInt32(dateTimePicker2.Value.Month.ToString());
+            int dayn = Convert.ToInt32(dateTimePicker2.Value.Day.ToString());
+            //Calculate Between 2 Dates
+            int years = (yearn - year);
+            int months = (years * 12 + (monthn - month));
+            int days = (months * 30 + (years * 12) +(dayn - day));
+            //Display Year,Month,Days...
+
+            string message = "You Have "+ years + " Year";
+            const string caption = "Year";
+            var result = MessageBox.Show(message, caption,
+                                         MessageBoxButtons.OK,
+                                         MessageBoxIcon.Information);
+            string messageh = "You Have" + months +" Month";
+            const string captionh = "Month";
+            var resulth = MessageBox.Show(messageh, captionh,
+                                         MessageBoxButtons.OK,
+                                         MessageBoxIcon.Information);
+            string messagem = "You Have"+ days +" Days";
+            const string captionm = "Days";
+            var resultm = MessageBox.Show(messagem, captionm,
+                                         MessageBoxButtons.OK,
+                                         MessageBoxIcon.Information);
+        }
+    }
+}
